47c95b36eaae7d890eca315d39be2b29e3c8a8b2
Christian Sadilek <christian.sadilek@gmail.com>
2020-05-05 16:19:57 -0400

    Upgrade to Android Components 41.0.20200505190119

--- app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import androidx.annotation.CallSuper
 import androidx.annotation.IdRes
@@ -77,6 +78,7 @@ import org.mozilla.fenix.utils.RunWhenReadyQueue
 import mozilla.components.concept.tabstray.TabsTray
 import mozilla.components.browser.tabstray.TabsAdapter
 import mozilla.components.browser.tabstray.BrowserTabsTray
+import mozilla.components.browser.tabstray.DefaultTabViewHolder
 import org.mozilla.fenix.tabtray.TabTrayFragmentDirections
 
 /**
@@ -218,7 +220,10 @@ open class HomeActivity : LocaleAwareAppCompatActivity() {
         }.asView()
         TabsTray::class.java.name -> {
             val layout = LinearLayoutManager(context)
-            val adapter = TabsAdapter(layoutId = R.layout.tab_tray_item)
+            val adapter = TabsAdapter { viewGroup, tabsTray ->
+                val view = LayoutInflater.from(context).inflate(R.layout.tab_tray_item, viewGroup, false)
+                DefaultTabViewHolder(view, tabsTray)
+            }
             BrowserTabsTray(context, attrs, tabsAdapter = adapter, layout = layout)
         }
         else -> super.onCreateView(parent, name, context, attrs)
